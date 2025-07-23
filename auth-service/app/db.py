27830f43393c9abf91b74c3e1265832fd43e7c5e
shoumik77 <shoumik.bisoi@gmail.com>
2025-07-23 10:06:25 -0400

    first commit

---
+++ auth-service/app/db.py
@@ -0,0 +1,13 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
